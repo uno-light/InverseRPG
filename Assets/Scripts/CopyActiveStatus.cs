365e023f1cfb9a341d19eafe27ef5eafbdde8ba9
ETdoFresh <ETdoFresh@gmail.com>
2019-10-28 14:06:01 -0500

    Change interface so that player clicks on monsters/heros/environment for context window to display good information.

---
+++ Assets/Scripts/CopyActiveStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyActiveStatus : MonoBehaviour
+{
+    public GameObject master;
+    public GameObject follower;
+
+    void Update()
+    {
+        follower.SetActive(master.activeSelf);
+    }
+}
---
+++ Assets/Scripts/Systems/ToggleOnPointerDown.cs
@@ -0,0 +1,23 @@
+﻿using ECSish;
+
+namespace InverseRPG
+{
+    public class ToggleOnPointerDown : MonoBehaviourSystem
+    {
+        private void Update()
+        {
+            foreach (var entity in GetEntities<OnPointerDownEvent, ToggleListener>())
+            {
+                var pointerDownEvent = entity.Item1;
+                var toggleListener = entity.Item2;
+                var toggle = toggleListener.GetComponent<ToggleGameObjectActiveEvent>();
+
+                foreach (var hideOnToggle in GetEntities<HideOnToggle>())
+                    if (hideOnToggle.Item1.gameObject != toggle.toggleGameObject)
+                        hideOnToggle.Item1.gameObject.SetActive(false);
+
+                toggle.toggleGameObject.SetActive(!toggle.toggleGameObject.activeSelf);
+            }
+        }
+    }
+}
